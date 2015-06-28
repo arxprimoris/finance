a29113fca222351858ed47007159325041eaaec7
Tom <tom@arxprimoris.net>
2015-06-28 16:01:47 -0700

    Added payment functionality

--- finance.py
+++ finance.py
@@ -5,9 +5,20 @@ Finance helper functions
 
 """
     Returns future value given current value and other parameters.
-"""
+"""    
 def futureValue(value, rate, periods = 1, payment = 0):
-    return value * ((1 + rate) ** periods)
-
+    total = value * ((1 + rate) ** periods)
+    for curPeriod in range(0, periods):
+        total = total + (payment * ((1 + rate) ** curPeriod))
+        
+    return total
+    
+"""
+    Returns present value given current value and other parameters.
+"""
 def presentValue(value, rate, periods = 1, payment = 0):
-    return value / (((1 + rate) ** periods))
+    total = value / ((1 + rate) ** periods)
+    for curPeriod in range(0, periods):
+        total = total + (payment / ((1 + rate) ** curPeriod))
+        
+    return total
